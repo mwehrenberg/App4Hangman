4955081a32a7853f7d5257056165f6b39b3dfc85
John Wehrenberg <John@johns-macbook-air.local>
2017-06-30 14:22:36 -0500

    Initial Commit

---
+++ App 4 - HangmanTests/App_4___HangmanTests.swift
@@ -0,0 +1,36 @@
+//
+//  App_4___HangmanTests.swift
+//  App 4 - HangmanTests
+//
+//  Created by John Wehrenberg on 6/30/17.
+//  Copyright © 2017 Molly Wehrenberg. All rights reserved.
+//
+
+import XCTest
+@testable import App_4___Hangman
+
+class App_4___HangmanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
