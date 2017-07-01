8824ac5faba3d90e05a9e1140892fefac2bf77ba
John Wehrenberg <John@johns-macbook-air.local>
2017-07-01 12:27:47 -0500

    Home Page

---
+++ App 4 - Hangman/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  App 4 - Hangman
+//
+//  Created by John Wehrenberg on 6/30/17.
+//  Copyright © 2017 Molly Wehrenberg. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
