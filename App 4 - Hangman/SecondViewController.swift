302f55cf557e47f1ecc2a993727e393e1ebc00cb
John Wehrenberg <John@johns-macbook-air.local>
2017-07-02 18:27:04 -0500

    Beginning to test letter buttons

--- App 4 - Hangman/SecondViewController.swift
+++ App 4 - Hangman/SecondViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var wordInUse: UILabel!
+    
+    var wordGenerator = [String]()
+    var randomIndex = 0
+    var wordUsedForCurrentGame = ""
+    var currentWordLetters = [Character]()
+    var numberOfCharactersInWord = 0
+    var emptyInitializer = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        wordGenerator = ["HAMILTONE", "JEFFERSON", "HEY", "WOWOWE", "YUPE"]
+        randomIndex = Int(arc4random_uniform(UInt32(wordGenerator.count)))
+        wordUsedForCurrentGame = wordGenerator[randomIndex]
+        currentWordLetters = [Character](wordUsedForCurrentGame.characters)
+        print(currentWordLetters)
+        numberOfCharactersInWord = wordUsedForCurrentGame.characters.count
+        emptyInitializer = String(repeating: "_ ", count: numberOfCharactersInWord)
+        wordInUse.text = emptyInitializer
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func testForLetterInWord(givenLetter : Character) {
+        if currentWordLetters.contains(givenLetter){
+            print("YES")
+        }
+    }
+    
+    @IBAction func onQ(_ sender: Any) {
+    }
+    @IBAction func onW(_ sender: Any) {
+    }
+    @IBAction func onE(_ sender: Any) {
+        testForLetterInWord(givenLetter: "E")
+    }
+    
 }
